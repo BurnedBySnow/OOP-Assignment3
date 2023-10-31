0d857fecb335137680d67a4904f548e03cae90db
Pontus Lundin <pontus.lundin@yh.nackademin.se>
2023-10-31 15:55:58 +0100

    added Button classes

--- src/Board.java
+++ src/Board.java
@@ -2,21 +2,55 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class Board extends JFrame implements ActionListener {
-    JPanel boardPanel = new JPanel();
+    JPanel mainPanel = new JPanel();
+    JPanel north = new JPanel();
+    JPanel south = new JPanel();
+    JPanel board = new JPanel();
+    ButtonHandler button = new ButtonHandler();
+    java.util.List<Button> buttonsList = button.getButtonsList();
+    JButton newGame = button.getNewGameButton();
 
-    public Board(){
-        this.add(boardPanel);
-        boardPanel.setLayout(new GridLayout(4, 4));
-        this.pack();
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
+    Board() {
+
+        this.add(mainPanel);
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(north, BorderLayout.NORTH);
+        mainPanel.add(south, BorderLayout.SOUTH);
+
+        north.add(board);
+        board.setLayout(new GridLayout(4, 4));
+
+        south.add(newGame);
+        newGame.addActionListener(this);
+        north.setBackground(Color.PINK);
+        south.setForeground(Color.PINK);
+
+        for (int i = 0; i < 16; i++) {
+            Button button = buttonsList.get(i);
+            board.add(button);
+        }
+
+
+        pack();
+        setTitle("15-Puzzle");
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == newGame) {
+            board.removeAll();
+            List<Button> shuffled = button.getButtonsList();
+            for (Button button : shuffled) {
+                board.add(button);
+            }
+            board.revalidate();
+            board.repaint();
+        }
     }
 }
